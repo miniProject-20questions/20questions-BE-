2435a2c64f13b8fcd58c0ed190117a8d4cf3bb92
cesdea <aprkvnem@gmail.com>
2022-08-15 10:26:09 +0900

    modify error

--- routers/user.router.js
+++ routers/user.router.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/user.controller');
 const loginmidlleware = require('../middlewares/login-middleware');
+const authMiddlewares = require('../middlewares/auth-middleware');
 const userController = new UserController();
+//test용 로그인 유저 확인
+router.get("/",authMiddlewares,userController.checkUser);
 //회원가입>>controller
 router.post('/signup', userController.userSignup);
 //로그인>>controller
