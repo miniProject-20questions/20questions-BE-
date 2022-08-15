e6964d29bf59b33886c530b2ac68435d3afead4f
cesdea <aprkvnem@gmail.com>
2022-08-15 10:56:02 +0900

    modify user.controller,user.service

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,20 +3,32 @@ require('dotenv').config();
 
 class UserController {
    userService = new UserService();
+
+   //로그인 중인 유저 정보확인
    checkUser=async(req,res,next)=>{
       res.json(res.locals.user);
    }
+
+   //회원가입
    userSignup = async (req, res, next) => {
       const { id, password, confirm ,nickname} = req.body;
+
       const upper = await this.userService.userSignup(id, password, confirm,nickname);
+
       return res.status(upper.status).json({
          message: upper.message,
       });
    };
+
+   //로그인
    userSignin = async (req, res, next) => {
       const { id, password} = req.body;
+
       const inner = await this.userService.userSignin(id, password);
+
+      //토큰 쿠키에 저장
       res.cookie(process.env.COOKIE_NAME, inner.token, { maxAge: 180000 });
+
       return res.status(inner.status).json({
          message: inner.message,
       });
