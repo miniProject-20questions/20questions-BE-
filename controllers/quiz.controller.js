69d32d7421529bb4f9b88a29185c610be0ba638c
Rumaro122 <yeshin122@gmail.com>
2022-08-15 18:37:34 +0900

    quiz.controller 기능 구현(테스트전)

--- controllers/quiz.controller.js
+++ controllers/quiz.controller.js
@@ -1,21 +1,86 @@
-const e = require('express');
-const joi = require('joi');
-const quizService = require('../services/quiz.service');
+const QuizService = require('../services/quiz.service');
 
 class QuizController {
     quizService;
 
     constructor() {
-        this.quizService = new this.quizService();
+        this.quizService = new QuizService();
     }
-
+    
+    // 퀴즈 포스트
     postQuiz = async (req,res) => {
-        const { title, category, answer } = req.body;
+        const { title, category, answer } = req.body;        
+        const { userId } = res.locals;
+        
+
+        try{
+            await joi.object ({
+                title: joi.string().required(),
+                category: joi.number().required(),
+                answer: joi.string().required(),
+                userId: joi.number().required(),
+        });
+
+            const result = await this.quizService.postQuiz(userId, category, answer, userId);
+
+            return res.status(200).json( result );
+        }catch(err){
+            
+            return res.status(400).send(err.message);;
+        }
+    };
+
+    //퀴즈 전체 조회
+    getQuiz = async (req,res) => {              
 
         try {
-            const result = await this.quizService.postQuiz();
+            const result = await this.quizService.getQuiz();
 
-            return res.status(200).json({ message : "퀴즈가 등록되었습니다" });            
+            return res.status(200).json({ result });            
+        } catch (err) {
+            console.log (err);
+
+            return res.status(500).json(err.message);
+        }
+    };
+
+    //퀴즈 상세 조회
+    getQuizById = async (req,res) => {   
+        const { quizId } = req.params;
+
+        try {
+            await joi
+                .object({
+                    quizId: joi.number().required(),
+                })
+                .validateAsync({ quizId });
+
+            const result = await this.quizService.getQuizById(quizId);
+
+            return res.json(result);
+        } catch (err) {
+            return res.json(err.message);
+        }
+    };
+
+    //퀴즈 삭제
+    deleteQuiz = async (req,res) => {    
+        const { userId } = res.locals;
+        const { quizId } = res.params;
+            
+
+        try {
+            await joi
+            .object ({
+                quizId: joi.number().required(),
+                userId: joi.number().required(),
+            })
+            .validateAsync({ quizId, userId });
+
+
+            const result = await this.quizService.deleteQuiz( quizId, userId );
+
+            return res.status(200).json(result);            
         } catch (err) {
             console.log (err);
 
