99d05b484a68fb68ee319ee3de5e874e782053c4
Rumaro122 <yeshin122@gmail.com>
2022-08-15 18:39:06 +0900

    quiz.router 수정 (테스트전)

--- routers/quiz.router.js
+++ routers/quiz.router.js
@@ -1,20 +1,25 @@
 const express = require('express');
+const router = express.Router();
+const authMiddlewares = require('../middlewares/auth-middleware');
+
 const quizRouter = express.Router();
-const quizController = require('../controllers/quiz.controller')
+router.use(authMiddlewares)
+
+const QuizController = require('../controllers/quiz.controller');
+const quizController = new QuizController();
 
 
+quizRouter
+    .route('')
+    .get(quizController.getQuiz)    
+    .post(quizController.postQuiz);
 
-// quizRouter
-//     .route('/api/quiz')
-//     .get(quizController.getQuiz)    
-//     .delete(quizController.deleteQuiz)
-
-// quizRouter
-//     .route('/:quizId')
-//     .get(quizController.getQuiz)    
-//     .post(quizController.postQuiz);
+quizRouter
+    .route('/:quizId')
+    .get(quizController.getQuizById)    
+    .delete(quizController.deleteQuiz);
 
 
-module.exports = quizRouter;
+module.exports = router;
 
 
