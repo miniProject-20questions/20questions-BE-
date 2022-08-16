20820abc08b5d62f30411514c100f66decbf3f38
Rumaro122 <yeshin122@gmail.com>
2022-08-16 14:18:41 +0900

    quiz.router add patch

--- routers/quiz.router.js
+++ routers/quiz.router.js
@@ -6,14 +6,15 @@ const QuizController = require('../controllers/quiz.controller');
 const quizController = new QuizController();
 
 //메인페이지(퀴즈 조회)
-router.get('/',  quizController.getQuiz);
+router.get('/', quizController.getQuiz);
 //메인페이지(퀴즈 등록)
 router.post('/', authMiddlewares, quizController.postQuiz);
 //상세페이지(퀴즈 삭제)
 router.delete('/:quizId', authMiddlewares, quizController.deleteQuiz);
 //상세페이지 (퀴즈 상세 조회)
 router.get('/:quizId', authMiddlewares, quizController.getQuizById);
-
+//상세페이지 (퀴즈 카테고리 수정)
+router.patch('/:quizId', authMiddlewares, quizController.updateCategory);
 
 
 module.exports = router;
